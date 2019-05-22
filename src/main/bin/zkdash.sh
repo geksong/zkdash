2c44147380616562e60fe5db23d369b4902e3d06
liubianshi <liubianshi@wacai.com>
2019-05-22 15:20:11 +0800

    fix ip and port textfiled

--- src/main/bin/zkdash.sh
+++ src/main/bin/zkdash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ZKDASH_VERSION=1.0.1
+ZKDASH_VERSION=1.0.2
 
 if [[ -d ${JAVA_HOME} ]];then
         ${JAVA_HOME}/bin/java -Djava.security.egd=file:/dev/./urandom -jar ./zkdash-${ZKDASH_VERSION}.jar > ./output.log 2>&1 &
